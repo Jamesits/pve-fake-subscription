cdbf08f354aec8bd9ac06a21b90cef794a37a869
James Swineson <github@public.swineson.me>
2019-06-22 23:22:10 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -5,7 +5,7 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 fpm -s dir -t deb \
 	-n pve-fake-subscription \
 	--description "Pollute Proxmox VE 5.x subscription cache so it won't alert you on dashboard login" \
-	--url "https://github.com/Jamesits/proxmox-ve-i-really-have-no-money-to-buy-a-subscription" \
+	--url "https://github.com/Jamesits/pve-fake-subscription" \
 	-v 0.0.1 \
 	--license "GLWTS(Good Luck With That Shit) Public License" \
 	--depends "python3" \

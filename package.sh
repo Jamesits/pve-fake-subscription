29ad6526696d081ee32012c30eff1192f0ad8f1d
James Swineson <github@public.swineson.me>
2019-09-01 06:01:32 -0700

    do not allow to install on non-pve systems; bump version to 0.0.3

--- package.sh
+++ package.sh
@@ -4,11 +4,12 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 fpm -s dir -t deb --force \
 	-n pve-fake-subscription \
-	--description "Pollute Proxmox VE 5.x subscription cache so it won't alert you on dashboard login" \
+	--description "Pollute Proxmox VE (>=5.0) subscription cache so it won't alert you on dashboard login" \
 	--url "https://github.com/Jamesits/pve-fake-subscription" \
-	-v 0.0.2 \
+	-v 0.0.3 \
 	--license "GLWTS(Good Luck With That Shit) Public License" \
 	--depends "python3" \
+	--depends "libpve-common-perl" \
 	--architecture all \
 	--deb-dist "unstable" \
 	--deb-priority "optional" \

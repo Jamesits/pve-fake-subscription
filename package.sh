86e774ab19ae518a46e6201acf70f81422d90b03
James Swineson <github@public.swineson.me>
2021-06-25 00:00:36 +0800

    remove dependence on libpve-common-perl since Proxmox Backup Server does not come with it

--- package.sh
+++ package.sh
@@ -4,12 +4,11 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 fpm -s dir -t deb --force \
 	-n pve-fake-subscription \
-	--description "Pollute Proxmox VE (>=5.0) and Proxmox Mail Gateway (>=5.0) subscription cache so it won't alert you on dashboard login" \
+	--description "Pollute the subscription cache of Proxmox VE (>=5.0), Proxmox Mail Gateway (>=5.0) & Proxmox Backup Server (>=1.0) so it won't alert you on dashboard login" \
 	--url "https://github.com/Jamesits/pve-fake-subscription" \
 	-v 0.0.5 \
 	--license "GLWTS(Good Luck With That Shit) Public License" \
 	--depends "python3" \
-	--depends "libpve-common-perl" \
 	--architecture all \
 	--deb-dist "unstable" \
 	--deb-priority "optional" \
